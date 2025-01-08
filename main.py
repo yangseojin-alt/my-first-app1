cbc4ddd4c4e28c1acfde865515f84eb85a6efbac
yangseojin-alt <Eunhoyang108@gmail.com>
2025-01-08 11:25:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.text('n/n')
 st.write('안녕하세요 나는 상우 짱친 양서진 입니다.')
 st.write('저의 이메일은 24_10513@daejin.sen.hs.kr')
 
-st.button("Reset", type="magenta")
+st.button("Reset", type="primary")
 if st.button("난수생성"):
     st.write(random.randint(1,1000))
 else:
